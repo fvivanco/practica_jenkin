90df12023b8661171ccabb55bcb6185a6d4629a6
unknown <fvivancoq@uni.pe>
2020-08-28 23:46:41 -0500

    nueva cambio a la chit

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("hola mundo");
-        System.out.println("La conexion con git se ha echooo");
+        System.out.println("La conexion con git se ha echooo0");
     }
 }

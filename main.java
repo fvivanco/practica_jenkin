20b5dad575c580eb1ba6b4ab4e09ad71a022dd9e
unknown <fvivancoq@uni.pe>
2020-08-28 21:34:36 -0500

    se cambio el mensaje

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
 class Main{
     public static void main(String[] args){
-        System.out.println("hola mundo");
-        System.out.println("La conexion con git se ha realizado
+        System.out.println(mama
     }
 }
